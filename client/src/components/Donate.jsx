0dc3d0b81417cf88dab579fabb731c2f40726311
raninder dhillon <raninderdhillon@gmail.com>
2022-01-21 15:27:03 -0500

    data in donate component aligned

--- client/src/components/Donate.jsx
+++ client/src/components/Donate.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./donate.css"
 import { useHistory } from 'react-router-dom';
 
- 
 
 export default function Donate(props) {
 	const [result, setResult] = useState({});
@@ -22,11 +22,24 @@ export default function Donate(props) {
 	console.log("id",id);
 	
 	return(
-		<div>
-			<h1>hello</h1>
-			{id}
-			Result {result.id}
-			<button onClick={() => history.push('/DonateForm')}>Donate</button>
+
+			<div className='container'>
+				
+      	<img
+					className= "image"
+          src={result.img}
+          alt={"sorry"}
+        />	
+				<div className="text">
+					id:{result.id} <br/>
+					User id: {result.user_id} <br/>
+					Cat id: {result.fund_cate_id} <br/>
+					Title: {result.title} <br/>
+					Goal: {result.goal} <br/>
+					Story: {result.story} <br/><br/>
+				
+			<button className="btn btn-primary" onClick={() => history.push('/DonateForm')}>Donate Now</button>
+			</div>
 		</div>
 	) 
 }
