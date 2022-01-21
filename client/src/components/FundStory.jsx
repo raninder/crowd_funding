00812ecba80e67444f2182b2e0b3227675e26f16
raninder dhillon <raninderdhillon@gmail.com>
2022-01-20 22:11:07 -0500

    component to display findraising story created

--- client/src/components/FundStory.jsx
+++ client/src/components/FundStory.jsx
@@ -1,29 +1,24 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useAlert } from 'react-alert';
+import Donate from "./Donate";
+// import { Button } from 'react-bootstrap';
+// import history from './history';
+import { useHistory } from 'react-router-dom';
+
 import {
-  
+  BrowserRouter as Router, 
+  Switch, 
+  Route,
   Link
 
 } from "react-router-dom";
 
 export default function FundStory(props) {
-	const [result, setResult] = useState(null);
 	const { id,user_id,fund_cate_id,title,img,story,goal } = props;
 
-	//  const alert = useAlert();
+	const history = useHistory();
+  
 
-	//  const url = "http://localhost:3001/api/funds/getallfundraising/"+id;
-	//  const fetchData = async () => {
-	// 	 const response = await axios.get(url);
- 
-	// 	 setResult(response.data);
-	//  };
-	// console.log("result",result);
-	// const fetchData = function(){
-	// 	return <
-	// }
-	
   return (
 		
 		<div className="products">
@@ -39,10 +34,14 @@ export default function FundStory(props) {
 					Title: {title} <br/>
 					Goal: {goal} <br/>
 					Story: {story} <br/>
-					{/* if user logged in */}
-					<Link to="/signup" className="btn btn-primary">Select</Link>
 
-					{/* <button onClick={() => { if (window.confirm('Are you sure you wish to donate?')) fetchData() }}>Select</button>  */}
+					<Link
+  				to={{
+   				 	pathname: "/Donate",
+    				state: { id }
+  				}}
+					className="btn btn-primary">Donate</Link>
+
 		</div>
 
 	);
