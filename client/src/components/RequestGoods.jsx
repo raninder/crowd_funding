f2eef7922c0caf2d9fd3c94bc9f070a3e7ac9354
raninder dhillon <raninderdhillon@gmail.com>
2022-01-22 13:06:20 -0500

    DRY search and RequestGoods components

--- client/src/components/RequestGoods.jsx
+++ client/src/components/RequestGoods.jsx
@@ -6,11 +6,12 @@ import "../styles/DispGoods.css";
 import { useHistory } from 'react-router-dom';
 
 
-
 export default function RequestGoods(props) {
   const [goods, setGoods] = useState([]);
+  const [filteredData,setFilteredData] = useState(goods);
   
   const history = useHistory();
+ 
 
   
   const url = "http://localhost:3001/api/goods/getallgoods";
@@ -19,23 +20,29 @@ export default function RequestGoods(props) {
     .then((response) => {
       console.log("data",response);
       setGoods(response.data);
+      // setFilteredData(response.data);
     })
     .catch(error => {
-      console.log('Error getting fake data: ' + error);
+      console.log('Error: ' + error);
       })
   }, []);
 
-
+  function handleSearch(event){
+    let value = event.target.value;
+    let result = [];
+    console.log(value);
+    result = goods.filter((data) => {
+      return data.good_cat_id==value;
+      });
+      console.log("result",result);
+    history.push('/Search', { result});
+  
+  }
 const allGoods = goods.map((item) => {
   console.log("items",item.id);
   return <Product key={item.id} {...item} />;
 });
 
-function handleSearch(event){
-  let value = event.target.value;
-  history.push('/Search', { value });
-
-}
 return (
   <>
     <p/><p/><p/><p/>
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -5,29 +5,9 @@ import Product from "./Product";
 
 
 export default function Search() {
-	const [goods, setGoods] = useState([]);
 
 	const history = useHistory();
-	const catid = history.location.state.value;
-
-	const url = "http://localhost:3001/api/goods/getallgoods";
-  useEffect(() => {
-    axios.get(url)
-    .then((response) => {
-      setGoods(response.data);
-    })
-    .catch(error => {
-      console.log('Error getting fake data: ' + error);
-      })
-  }, []);
-
-	let result1 = [];
-    console.log("value",catid);
-    
-    result1 = goods.filter((data) => {
-    return data.good_cat_id==catid;
-    });
-    console.log("result", result1);
+	const result1 = history.location.state.result;
 
 		const allGoods = result1.map((item) => {
 			console.log("items",item.id);
